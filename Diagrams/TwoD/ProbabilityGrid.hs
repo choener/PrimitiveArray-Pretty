59bfbd3f437efffb66b30758c2a47ba96771aa95
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2016-03-13 23:14:59 +0100

    renamed to more useful module structure

--- Diagrams/TwoD/ProbabilityGrid.hs
+++ Diagrams/TwoD/ProbabilityGrid.hs
@@ -1,27 +1,16 @@
 
--- | Sensible prettyfication of Inside/Outside combinations without having
--- to invoke a third grammar (that would be the same for many problems
--- anyway).
+-- | Probability grid square drawing routines.
 
-module Data.PrimitiveArray.Pretty.InOut where
+module Diagrams.TwoD.ProbabilityGrid where
 
---import Graphics.Rendering.Chart.Easy
---import Graphics.Rendering.Chart.Backend.Cairo
-import Numeric.Log
-import Diagrams.TwoD
-import Diagrams.Prelude
-import Data.List.Split (chunksOf)
-import Diagrams.Backend.SVG
-import Diagrams.Backend.Postscript
 import Data.List (genericLength)
-
-
-
---toPlot :: (Default r, ToRenderable r) => [(Int,Int,Log Double)] -> EC r ()
---toPlot xs = do
---  layout_title .= "Match Probabilities"
---  plot $ liftEC $ do
---    undefined
+import Data.List.Split (chunksOf)
+import Diagrams.Backend.Postscript
+import Diagrams.Backend.SVG
+import Diagrams.Prelude
+import Diagrams.TwoD
+import Diagrams.TwoD.Text
+import Numeric.Log
 
 
 
@@ -33,6 +22,9 @@ data FillWeight = FWlog | FWlinear
 -- | A single square in our grid.
 
 -- gridSquare :: FillWeight -> Log Double
+gridSquare
+  :: (Monoid m, Semigroup m, TrailLike (QDiagram b V2 Double m))
+  => FillWeight -> Log Double -> QDiagram b V2 Double m
 gridSquare (fw :: FillWeight) (v :: Log Double) = g `beneath` (z # scale s)
   where s = case fw of
               FWlog     -> 1 / (1 - ln v)
@@ -40,19 +32,35 @@ gridSquare (fw :: FillWeight) (v :: Log Double) = g `beneath` (z # scale s)
         z = square 1 # lw 0 # fc blue # centerXY
         g = square 1 # lc black
 
+-- | Draw the actual grid.
+
+grid
+  :: ( Renderable (Diagrams.TwoD.Text.Text Double) b
+     , Renderable (Path V2 Double) b)
+  => FillWeight
+  -> t
+  -> Int
+  -> [String]
+  -> [String]
+  -> [Log Double]
+  -> QDiagram b V2 Double Any
 grid (fw :: FillWeight) n m (ns :: [String]) (ms :: [String]) (vs :: [Log Double])
---  | null ns   = vcat $ map hcat $ map (map (gridSquare fw)) $ chunksOf n $ vs
---  | length ns /= n = error "grid: annotation length not equal to grid size"
   | null ns && null ms = grd
   | otherwise =  (grd ||| ns') === ms'
   where ns' = if null ns then mempty else vcat $ map (\t -> (square 1) `beneath` (text t # scale (0.9 / genericLength t))) ns
         ms' = if null ms then mempty else hcat $ map (\t -> (square 1) `beneath` (text t # scale (0.9 / genericLength t))) ms
         grd = vcat $ map hcat $ map (map (gridSquare fw)) $ chunksOf m $ vs
 
+-- | Render as @svg@.
+
+svgGridFile :: FilePath -> FillWeight -> Int -> Int -> [String] -> [String] -> [Log Double] -> IO ()
 svgGridFile fname fw n m ns ms vs = renderPretty fname size $ g
   where size = ((*100) . fromIntegral) <$> mkSizeSpec2D (Just m) (Just n) -- Nothing Nothing -- n n
         g = grid fw n m ns ms vs
 
+-- | Render as @eps@.
+
+epsGridFile :: String -> FillWeight -> Int -> Int -> [String] -> [String] -> [Log Double] -> IO ()
 epsGridFile fname fw n m ns ms vs = renderDia Postscript (PostscriptOptions fname size EPS) g
   where size = ((*100) . fromIntegral) <$> mkSizeSpec2D (Just m) (Just n)
         g = grid fw n m ns ms vs
