83f91f79db4b5adbe3dbbd8c8dc89dbc96be2f8e
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2019-03-04 00:16:27 +0100

    move to envhs

--- default.nix
+++ default.nix
@@ -1,2 +1,21 @@
-with (import <nixpkgs> {});
-hsDevFunctions ./.
+{ mkDerivation, base, containers, diagrams, diagrams-contrib
+, diagrams-lib, diagrams-postscript, diagrams-svg, filepath
+, log-domain, QuickCheck, smallcheck, split, stdenv, tasty
+, tasty-quickcheck, tasty-smallcheck, tasty-th
+}:
+mkDerivation {
+  pname = "PrimitiveArray-Pretty";
+  version = "0.1.0.0";
+  src = ./.;
+  libraryHaskellDepends = [
+    base containers diagrams diagrams-contrib diagrams-lib
+    diagrams-postscript diagrams-svg filepath log-domain split
+  ];
+  testHaskellDepends = [
+    base QuickCheck smallcheck tasty tasty-quickcheck tasty-smallcheck
+    tasty-th
+  ];
+  homepage = "https://github.com/choener/PrimitiveArray-Pretty";
+  description = "Pretty-printing for primitive arrays";
+  license = stdenv.lib.licenses.bsd3;
+}
